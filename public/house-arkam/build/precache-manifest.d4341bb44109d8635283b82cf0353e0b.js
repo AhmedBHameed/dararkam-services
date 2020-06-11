fe031edfed496f6b3fbdd6f4c9c2d17e6429b68c
AhmedBHameed <ahmedbazy@gmail.com>
2020-06-11 16:58:56 +0200

    push application build files

---
+++ public/house-arkam/build/precache-manifest.d4341bb44109d8635283b82cf0353e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40803238057c8ba5276107b21c22bc7",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/index.html"
+  },
+  {
+    "revision": "44474890170d61f9ab14",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7378f34ec7d83258dce5",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/static/css/main.34409ce2.chunk.css"
+  },
+  {
+    "revision": "44474890170d61f9ab14",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/static/js/2.314c5ac0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/static/js/2.314c5ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7378f34ec7d83258dce5",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/static/js/main.d0612211.chunk.js"
+  },
+  {
+    "revision": "c51b314b727a5b3f98cf",
+    "url": "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/static/js/runtime-main.918035e9.js"
+  }
+]);
---
+++ public/house-arkam/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/precache-manifest.d4341bb44109d8635283b82cf0353e0b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prayers-list/01EAG4SCJSN7HXE500X0W0YEKK01EAG4SCJSXH9KTAGRTDEXF06E/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
